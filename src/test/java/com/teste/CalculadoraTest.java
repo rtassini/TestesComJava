784ba8ae9df09c6cac48342de9c4a485550e3289
Rodrigo Tassini <rodrigo.tassini@gmail.com>
2019-10-21 16:24:18 -0300

    Exemplo simples mockando objeto

--- src/test/java/com/teste/CalculadoraTest.java
+++ src/test/java/com/teste/CalculadoraTest.java
@@ -1,6 +1,9 @@
 package com.teste;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 import org.junit.Test;
 
@@ -11,6 +14,19 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma = calc.somar("1+1+2");
 		assertEquals(4, soma);
+	}
+	
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2")).thenReturn(10);
+		
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(10, resultado);
+		
+		
 	}
 
 }
